9a9b0a1df1849431e9d4b29da7a28a593ac3db02
Muhammad <hamzasiddiqu007@gmail.com>
2025-08-31 23:34:35 -0700

    explore grid model

---
+++ lib/model/explore_grid_model.dart
@@ -0,0 +1,13 @@
+class GridItemModel {
+final String title;
+final String image;
+final String buttonText;
+final int buttonColor;
+
+GridItemModel({
+  required this.title,
+  required this.image,
+  required this.buttonText,
+  required this.buttonColor,
+});
+}
